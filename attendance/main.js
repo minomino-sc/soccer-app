d626367108161105da724cd11db13b70a100cbd7
minomino-sc <nino388suaki@gmail.com>
2025-12-29 21:15:53 +0900

    Update main.js

--- attendance/main.js
+++ attendance/main.js
@@ -5,9 +5,9 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "★★★★★",
+  authDomain: "★★★★★",
+  projectId: "minotani-sc-app",
 };
 
 const app = initializeApp(firebaseConfig);
