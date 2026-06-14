c828b09e28c7413a9dadbbced297b384bc2d7799
minomino-sc <nino388suaki@gmail.com>
2026-06-14 20:10:00 +0900

    Update event.js

--- haisha/event.js
+++ haisha/event.js
@@ -1,1 +1,9 @@
 console.log("event loaded");
+
+document.getElementById("eventDetail").innerHTML = `
+  <div class="event-card">
+    <div class="event-title">
+      event.js 動作成功
+    </div>
+  </div>
+`;
