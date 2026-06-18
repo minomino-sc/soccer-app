662edc6b30ad2740e05b5f8b0e44461cdc13f6e1
minomino-sc <nino388suaki@gmail.com>
2026-06-19 07:52:03 +0900

    Update dispatch.js

--- haisha/dispatch.js
+++ haisha/dispatch.js
@@ -1,3 +1,10 @@
+import { db } from "./firebase.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const params =
   new URLSearchParams(
     window.location.search
@@ -6,9 +13,42 @@ const params =
 const id =
   params.get("id");
 
-document.getElementById(
-  "dispatchArea"
-).innerHTML = `
-  イベントID<br>
-  ${id}
-`;
+const snap =
+  await getDoc(
+    doc(
+      db,
+      "car_dispatch_events",
+      id
+    )
+  );
+
+if (!snap.exists()) {
+
+  document.getElementById(
+    "dispatchArea"
+  ).innerHTML =
+    "イベントが見つかりません";
+
+}
+else {
+
+  const data =
+    snap.data();
+
+  document.getElementById(
+    "dispatchArea"
+  ).innerHTML = `
+
+    <h2>${data.title}</h2>
+
+    <div>
+      日付：${data.date}
+    </div>
+
+    <div>
+      対象：${data.target}
+    </div>
+
+  `;
+
+}
