00f8fdb842634edf99fa11257a0414286435fba5
minomino-sc <nino388suaki@gmail.com>
2026-06-26 21:42:23 +0900

    Update initDriverCount.js

--- haisha/initDriverCount.js
+++ haisha/initDriverCount.js
@@ -1,1 +1,38 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  setDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import {
+  COACH_A,
+  COACH_B
+} from "./players.js";
+
+const allDrivers = [
+  ...COACH_A,
+  ...COACH_B
+];
+
+// 重複削除
+const drivers =
+  [...new Set(allDrivers)];
+
+for (const name of drivers) {
+
+  await setDoc(
+    doc(
+      db,
+      "driver_counts",
+      name
+    ),
+    {
+      count: 0
+    }
+  );
+
+}
+
+alert("初期登録完了");
